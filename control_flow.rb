5b451159cfe6ab701e0294c635b2ac5b5f47d56e
Yaneizy Castillo <yccastillo18@gmail.com>
2021-09-20 17:02:01 -0400

    Completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,69 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+
+  end
+  # if the username == admin
+    # the password will be "12345"
+    # and say "Access granted"
+    # otherwise say "access denied"
 end
 
+
+
+
+ 
 def hows_the_weather(temperature)
-  # your code here
+  response = if temperature < 40 
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65 
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else 
+    "It's perfect out there!"
+  end
+
+#   40>= "It's brisk out there!"
+#   40<=65 "It's a little chilly out there!"
+#   85>= "It's too dang hot out there!"
+# else "It's perfect out there!"  
 end
+
+
 
 def fizzbuzz(num)
-  # your code here
+ if num % 3 == 0 && num % 5 == 0
+  "FizzBuzz"
+ elsif num % 3 == 0
+  "Fizz"
+ elsif num % 5 == 0
+  "Buzz"
+ else
+   num
+ end
+
+  # multiples of 3 == "Fizz"
+  # multiples of 5 == "Buzz"
+  # 3 x 5 == "FizzBuzz" 
+  # any other num is just the num
+
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else
+    puts "Invalid operation!"
+  end
 end
 
